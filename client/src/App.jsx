3a42798234bca864f09f6608fac0a17b9396fcdd
SinghCtn <chetansingh2k1@gmail.com>
2024-10-03 06:13:56 +0530

    done client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useQuery, gql } from "@apollo/client";
+
+const query = gql`
+  query GetTodosWithUser {
+    getTodos {
+      id
+      title
+      completed
+      user {
+        id
+        name
+      }
+    }
+  }
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { data, loading } = useQuery(query);
+  if (loading) return <h1> Loading.... </h1>;
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <table>
+        {data.getTodos.map((todo) => (
+          <tr key={todo.title}>
+            <td>{todo.title}</td> <td>{todo.user.name}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
 }
 
-export default App
+export default App;
